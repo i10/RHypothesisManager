ee3815970125d0aa2699f85ebcea45bd90ef6eca
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-07-02 15:07:21 +0200

    Make snapshot tests

--- tests/testthat/test-parser.R
+++ tests/testthat/test-parser.R
@@ -1,56 +1,30 @@
+uuidCounter <<- 0
+UUIDgenerate <<- function () {
+    uuidCounter <<- uuidCounter + 1
+    uuidCounter
+}
+
 test_that("parsing works on examples", {
-  expect_parse <- function (code, expected_variables, expected_functions, expected_hypotheses) {
-    eval_ <<- FALSE;
-    c(variables, functions, hypotheses) %<-% parse(text=code, line_non=1, interactive=FALSE, variables=NULL, functions=NULL, hypotheses=NULL);
+  expect_parse <- function (fileName) {
+    path <- paste0("../snapshots/", fileName, ".R")
+    code <- readChar(path, file.info(path)$size)
+
+    eval_ <<- FALSE
+    c(variables, functions, hypotheses) %<-% parse(text=code, line_non=1, interactive=FALSE, variables=NULL, functions=NULL, hypotheses=NULL)
+
+    c(expected_variables, expected_functions, expected_hypotheses) %<-% readRDS(paste0(path, "-snapshot"))
     
-    variables$id <- NULL;
-    expected_variables$id <- NULL;
-    expect_equal(variables, expected_variables);
-    functions$id <- NULL;
-    expected_functions$id <- NULL;
-    expect_equal(functions, expected_functions);
-    hypotheses$id <- NULL;
-    expected_hypotheses$id <- NULL;
-    expect_equal(hypotheses, expected_hypotheses);
+    expected_variables$id <- variables$id
+    variables$precursor <- NULL
+    expected_variables$precursor <- NULL
+    expect_equal(variables, expected_variables)
+    expected_functions$id <- functions$id
+    expect_equal(functions, expected_functions)
+    expected_hypotheses$id <- hypotheses$id
+    expect_equal(hypotheses, expected_hypotheses)
   }
 
-  # Helpers
-  make_variables <- function (variables) {
-    variables <- data.frame(matrix(variables, ncol = 8, nrow = 0));
-    colnames(variables) <- c("id", "name", "precursors", "columns", "origin", "type", "value", "generation");
-    return(variables);
-  }
-
-  v <- function(id, name="a", precursors=NULL, columns=NULL, origin=NA, type="constant", value=NA, generation=0) {
-    if (missing(id)) {
-      id <- UUIDgenerate();
-    }
-    return(c(id, name, precursors, columns, origin, type, value, generation))
-  }
-
-  make_functions <- function () {
-    functions <- data.frame(matrix(ncol = 8, nrow = 0));
-    colnames(functions) <- c("id", "name", "lines", "signature", "packages", "arguments", "depth", "breakpoint");
-    return(functions);
-  }
-
-  make_hypotheses <- function () {
-    hypotheses <- data.frame(matrix(ncol = 6, nrow = 0));
-    colnames(hypotheses) <- c("id", "name", "columns", "functions", "models", "formulas");
-    return(hypotheses);
-  }
-
-  # Test cases
   expect_parse(
-    "",
-    make_variables(),
-    make_functions(),
-    make_hypotheses()
-  );
-  expect_parse(
-    "a <- 1",
-    make_variables(c(v(name="a"))),
-    make_functions(),
-    make_hypotheses()
+    "intermediate-kbd-study"
   );
 })
